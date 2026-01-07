ea0b8d5e4ac5cc5b7549f519433da40f7da64c22
Rei.K <rei.engineer.hub@gmail.com>
2026-01-07 21:15:14 +0900

    エネミーコントローラーをステートマシーン化

---
+++ Assets/Programs/Runtime/Core/Constants/LayerMaskConstants.cs
@@ -0,0 +1,7 @@
+namespace Game.Core.Constants
+{
+    public static class LayerMaskConstants
+    {
+        public static readonly int PlayerLayerMask = 1 << UnityEngine.LayerMask.NameToLayer("Player");
+    }
+}
